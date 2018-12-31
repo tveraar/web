5e616a775a0b69855a130f664af2553e4566ef2f
tveraar <45404746+tveraar@users.noreply.github.com>
2018-12-31 13:57:09 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,22 @@
+/*jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true/
+/eslint-env browser/
+/eslint 'no-console':0*/
+
+
+/* laat formulier verschijnen, bron: Rico Zethof */
+var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+
+if (w < 950) {
+    document.getElementById("form").style.display = "none";
+}
+
+function showFilters() {
+    var x = document.getElementById("form");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+
+document.getElementById("filter").addEventListener("click", showFilters);
